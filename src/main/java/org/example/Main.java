f9b8c24f2c7535a3f358cdd3de49289efb414d74
jose <jomoespe@gmail.com>
2023-03-13 10:26:23 +0100

    Move connection creation to try with resources

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,22 +11,17 @@ public class Main {
         String username = "postgres";
         String password = "postgrespw";
 
-        try {
-            Connection connection = DriverManager.getConnection(jdbcURL, username, password);
+        try (Connection connection = DriverManager.getConnection(jdbcURL, username, password)){
             System.out.println("Connected to PostgreSQL server");
-
             String sql = "INSERT INTO person (personid, firstname, secondname, address, city)" + " VALUES (1, 'Lionel', 'Messi', 'PSG', 'Argentina')";
-
             Statement statement = connection.createStatement();
 
             int rows = statement.executeUpdate(sql);
             if (rows > 0) {
                 System.out.println("A new person has been added.");
             }
-
-            connection.close();
+            statement.close();
         } catch (SQLException e) {
-            System.out.println("Error connecting to PostgreSQL DB");
             e.printStackTrace();
         }
     }
